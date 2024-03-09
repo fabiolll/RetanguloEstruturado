0f695c392cb4b1f52bb82733b42ff77308564de0
Fabiolll <fabio95.df@gmail.com>
2024-03-09 04:39:33 -0300

    programa feito

--- Program.cs
+++ Program.cs
@@ -4,8 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
+       double largura, altura, area, perimetro, diagonal;
         Console.WriteLine("Vamos calcular as propriedades do triangulo!");
-        
+        Console.WriteLine("Informe a Largura: ");
+        largura = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Informe a Altura: ");
+        altura = Convert.ToDouble(Console.ReadLine());
+        area = largura*altura;
+        perimetro = 2*(largura)+2*(altura);
+        diagonal = Math.Sqrt((altura*altura)+(largura*largura));
+        Console.WriteLine($"Largura:{largura}\nAltura: {altura}\nArea: {area}\nPerimetro: {perimetro}\nDiagonal: {diagonal}");
 
     }
 }
--- obj/Debug/net8.0/RetanguloEstruturado.AssemblyInfo.cs
+++ obj/Debug/net8.0/RetanguloEstruturado.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RetanguloEstruturado")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d1860149fe661a4d6ff043d2f479e29b13775f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("RetanguloEstruturado")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RetanguloEstruturado")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
